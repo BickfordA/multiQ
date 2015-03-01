30077b02b2c4e1a82ec0ab6235266b29f14ee771
BickfordA <aidan.bickford@gmail.com>
2015-03-01 14:55:21 -0700

    Implemented Histogram

--- src/multiQ/histogram.cpp
+++ src/multiQ/histogram.cpp
@@ -1,20 +1,171 @@
 #include "histogram.h"
 
+#include <algorithm>
+#include <assert.h>
 
+using namespace std;
 
-
-Histogram::Histogram()
+double kdeKernel(double x)
 {
-
+	double f = 1 - (x * x);
+	return 0.9375 * f * f;
 }
 
-void Histogram::addValue(double value)
+Histogram::Histogram(vector<double> values, double kernelWidth, double bucketWidth)
+	: _bucketWidth(bucketWidth), _kernelWidth(kernelWidth)
 {
+	assert(!values.empty());
+	//sort the values so that we know where the smallest value is
+	sort(values.begin(), values.end());
 
+	if (_bucketWidth < 0)
+		_bucketWidth = _kernelWidth / -_bucketWidth;
+
+
+	_left = values[0] - kernelWidth - 1.5 * bucketWidth;
+
+	int bucketCount = (int)((values[values.size() - 1] + kernelWidth + 1.5* bucketWidth - _left) / bucketWidth) + 3; //why three? start stop and ??? middle???
+
+	_totalCount = values.size();
+
+	//first bin is empty ( multiplied by -1.5 for finding _left)
+
+
+	_buckets.push_back(Bucket(pos(0)-_kernelWidth, pos(0) + _kernelWidth, 0));
+
+	//thes are used to mark the begin and end
+	//of the section of values which
+	//belong to a given bin
+	int startBin = 0;
+	int stopBin = 0;
+
+	double inverseWidth = 1 / _kernelWidth;
+
+	for (int i = 1; i < bucketCount; i++)
+	{
+		//find the bucket bound in the input vector
+		double buckPos = _left + (i  * _bucketWidth);
+		
+		double currentStart = buckPos - _kernelWidth;
+		double currentStop = buckPos + _kernelWidth;
+
+		while (values[startBin] < currentStart && startBin < values.size()){
+			startBin++;
+		}
+
+		while (values[stopBin] < currentStart && stopBin< values.size()){
+			stopBin ++;
+		}
+
+		//calculate the probability for the current bucket given its points
+		double prob = 0;
+		for (int idx = startBin; idx < stopBin; idx++){
+			prob += kdeKernel((values[idx] - buckPos) *inverseWidth);
+		}
+		_buckets.push_back(Bucket(currentStart, currentStop, prob));
+
+		currentStart = currentStart;
+	}
 }
 
-double Histogram::interArrivalTime()
+double Histogram::kdeProbability(int bucketIdx)
 {
+	return( _buckets[bucketIdx].count / (_totalCount * _kdeWidth));
+}
 
-	return -1;
+double Histogram::kdeSigProb(int bucketIdx)
+{
+	return( sqrt(_buckets[bucketIdx].count) / _totalCount * _kdeWidth);
+}
+/*
+* A mode is the highest point in any region statistically more likely than one before it
+* and after it.
+*
+* This is calculated in three passes
+* 1: Find all local maxima
+* 2: Mark the significant points
+* 3: Combine duplicate points
+*/
+vector<int> Histogram::modes(double significance, int minPoints)
+{
+	// Find local maxima 
+	vector<Mode> modes;
+
+	vector<Mode> potentialMode;
+
+	for (int buckIdx = 0; buckIdx < _buckets.size(); buckIdx){
+		//find the left hand neighbor value
+		double leftDelta = (buckIdx == 0) ? -1 : _buckets[buckIdx - 1].count - _buckets[buckIdx].count;
+		
+		//find the right hand neighbor value
+		double rightDelta = (buckIdx + 1 == _buckets.size()) ? -1 : _buckets[buckIdx + 1].count - _buckets[buckIdx].count;
+		
+		if (leftDelta <= 0) //freq to the left was less or equal to current freq
+			potentialMode.push_back(Mode(buckIdx));
+		
+		if (rightDelta > 0) // the next mode increases
+			potentialMode.clear();
+
+		if (rightDelta < 0){ //the next mode decreases 
+			for (Mode m : potentialMode)
+				modes.push_back(m);
+		}
+	}
+
+
+
+	//bracket each mode by expanding the bounds (a,b)
+	for (vector<Mode>::iterator m = modes.begin(); m != modes.end(); m++) {
+		double prob = kdeProbability(m->i);
+
+		double probThresh = max(prob - significance * kdeSigProb(m->i), 0.0);
+
+		bool killMode = false;
+		for (m->a = (m->i - 1); m->a >= 0 && kdeProbability(m->a) > probThresh; m->a--){
+			if (kdeProbability(m->a) > 0){ //earlier max with ono dip
+				killMode = true;
+				break;
+			}
+		}
+
+		for (m->b = m->i + 1; m->b < _buckets.size() && kdeProbability(m->b) > probThresh && killMode; m->b++){
+			if (kdeProbability(m->b) > prob){ // later max with no dip
+				killMode = true;
+			}
+		}
+
+		if (killMode){
+			m->i = -1;
+		}
+	}
+
+	//combine overlapping modes to "mode of modes"
+
+	// coalesce overlapping modes into "mode of modes" (i.e. tallest)
+	{
+		std::sort(modes.begin(), modes.end());
+		vector<Mode>::iterator m = modes.begin();
+		// We sorted dead modes to the end of the list, so exit when we
+		// encounter one.  (We kill some modes ourselves, but we skip them
+		// right away.)
+		for (vector<Mode>::iterator next_m = m + 1; next_m < modes.end() && next_m->i >= 0; next_m++) {
+			if (next_m->i < m->b) { // next mode inside
+				if (kdeProbability(next_m->i) > kdeProbability(m->i))
+					m->i = next_m->i;
+				if (next_m->b > m->b)
+					m->b = next_m->b;
+				next_m->i = -1;
+			}
+			else
+				m = next_m;
+		}
+	}
+
+	vector<int> modeBucketIdx;
+	// sort modes in decreasing order of importance
+	for (vector<Mode>::iterator m = modes.begin(); m < modes.end(); m++)
+		if (m->i >= 0)
+			modeBucketIdx.push_back(m->i);
+
+	return modeBucketIdx;
 }
--- src/multiQ/histogram.h
+++ src/multiQ/histogram.h
@@ -3,17 +3,85 @@
 
 #include "distribution.h"
 
+#include <vector>
 
+//
+// Histogram
+//
 class Histogram : public Distribution
 {
 public: 
-	Histogram();
+	Histogram(std::vector<double> values, double width, double bucketWidth = -18.0);
 
-	void addValue(double value);
 
-	double interArrivalTime();
+	std::vector<int> modes(double significance, int minPoints);
+
+	double kdeProbability(int bucketIdx);
+	double kdeSigProb(int bucketIdx);
+
+
+	double pos(int i) const { return _left + i * _bucketWidth; }
+protected:
+
+	std::vector<Bucket> _buckets;
+
+	double _totalCount;
+	double _kdeWidth;
+
+	double _bucketWidth;
+	double _kernelWidth;
+
+	double _left; //value of left edge of smallest bucket
+};
+
+//
+// Bucket 
+// - holds the intervals of the histogram
+//
+
+struct Bucket
+{
+	Bucket(int start, int stop, int count)
+		:start(start), stop(stop), count(count)
+	{
+	};
+
+	double start;
+	double stop;
+
+	int count;
 };
 
 
+//
+// Mode 
+// - used for computing the modes in the histogram
+//
+
+namespace
+{
+	struct Mode
+	{
+		inline Mode(int idx)
+			:a(idx), b(idx), i(idx)
+		{
+		};
+
+		int a, i, b;
+	};
+
+	inline bool
+		operator<(const Mode &a, const Mode &b)
+	{
+		if (a.i < 0 || b.i < 0)
+			return b.i < a.i;
+		else if (a.a != b.a)
+			return a.a < b.a;
+		else if (a.i != b.i)
+			return a.i < b.i;
+		else
+			return a.b < b.b;
+	}
+}
 
 #endif //HISTOGRAM_H
